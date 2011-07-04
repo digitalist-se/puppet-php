8464166607d4aabee1b0be8cb9b46df6735822dc
Fabian Sorqvist <fabian.sorqvist@gmail.com>
2011-07-04 16:01:29 +0200

    Moves files around a bit

---
+++ manifests/php.pp
@@ -0,0 +1,45 @@
+class php (
+  $module_root = '/tmp/vagrant-puppet/manifests/modules/php',
+  $packages = [
+    'php5',
+    'php5-mysql',
+    'php5-gd',
+    'php-apc',
+    'php5-curl',
+    'php5-dev',
+    'php5-dbg',
+    'php5-xdebug'
+  ],
+  $conf_files = [
+    'apache2/php.ini',
+    'cli/php.ini',
+    'conf.d/apc.ini',
+    'conf.d/curl.ini',
+    'conf.d/gd.ini',
+    'conf.d/memcache.ini',
+    'conf.d/mysqli.ini',
+    'conf.d/mysql.ini',
+    'conf.d/pdo.ini',
+    'conf.d/pdo_mysql.ini',
+    'conf.d/security',
+    'conf.d/uploadprogress.ini',
+    'conf.d/xdebug.ini'
+  ]
+) {
+  package { $packages:
+    ensure => installed,
+  }
+
+  conf_file { $conf_files:
+    require => Package['php5']
+  }
+
+  define conf_file() {
+    file { "/etc/php5/${name}":
+      owner => root,
+      group => root,
+      mode => 0444,
+      source => "${module_root}/files/${name}"
+    }
+  }
+}
