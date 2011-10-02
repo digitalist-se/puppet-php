a9c74653ca565f491c4ac22e42a0e026ea4725e4
Fabian Sorqvist <fabian.sorqvist@gmail.com>
2011-10-02 11:35:50 +0200

    Proper shell test for checking pecl packages

--- manifests/definitions/pecl_package.pp
+++ manifests/definitions/pecl_package.pp
@@ -2,6 +2,6 @@ define php::pecl_package() {
   exec { "pecl-install-${name}":
     command => "/usr/bin/pecl install $name",
     require => Package['php-pear', 'build-essential', 'php5-dev'],
-#    unless => "/usr/bin/test -n '/usr/bin/pecl list | grep $name'"
+    unless => "/usr/bin/pecl shell-test $name"
   }
 }
