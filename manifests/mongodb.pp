e21dbd119b7977a6bbc8dca77855f48049b87076
Patrik Lembke <patrik.lembke@wunderkraut.se>
2012-11-20 13:31:16 +0100

    fixes a tiny typo

--- manifests/mongodb.pp
+++ manifests/mongodb.pp
@@ -1,7 +1,7 @@
 class php::mongo () {
   php::pecl_package { "mongo": }
 
-  file { "/etc/php5/conf.d/20-mongo":
+  file { "/etc/php5/conf.d/20-mongo.ini":
     owner  => root,
     group  => root,
     mode   => 0700,
