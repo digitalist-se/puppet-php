fe0b905e3a074a7499f608be234a46f9acf3a508
Patrik Lembke <patrik.lembke@wunderkraut.se>
2012-11-20 16:19:51 +0100

    mongofix

--- manifests/mongodb.pp
+++ manifests/mongodb.pp
@@ -1,10 +1,10 @@
 class php::mongo () {
   php::pecl_package { "mongo": }
 
-  file { "/etc/php5/conf.d/20-mongo.ini":
+  file { "/etc/php5/conf.d/mongo.ini":
     owner  => root,
     group  => root,
-    mode   => 0700,
+    mode   => 0444,
     content => "extension=mongo.so",
     require => php::pecl_package['mongo'],
   }
